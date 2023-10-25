565323923d7e62599ed1f80a8b91b87bcbe271ee
Sebastian Stulz <s.stulz@gmx.de>
2023-10-25 08:52:08 +0200

    Update cloudwatchfullaccessV2_update.sh
    typo fix

--- aws/cloudwatchfullaccessV2_update.sh
+++ aws/cloudwatchfullaccessV2_update.sh
@@ -2,7 +2,7 @@
 ### Update attached entities to latest policy version as announced by AWS
 
 OLD_POLICY_ARN="arn:aws:iam::aws:policy/CloudWatchFullAccess"
-NEW_POLCIY_ARN="arn:aws:iam::aws:policy/CloudWatchFullAccessV2"
+NEW_POLICY_ARN="arn:aws:iam::aws:policy/CloudWatchFullAccessV2"
 IAM_ROLES=( $(aws iam list-entities-for-policy --policy-arn ${OLD_POLICY_ARN} | jq -r '.PolicyRoles[].RoleName') )
 IAM_USERS=( $(aws iam list-entities-for-policy --policy-arn ${OLD_POLICY_ARN} | jq -r '.PolicyUsers[].UserName') )
 IAM_GROUPS=( $(aws iam list-entities-for-policy --policy-arn ${OLD_POLICY_ARN} | jq -r '.PolicyGroups[].GroupName') )
@@ -11,7 +11,7 @@ IAM_GROUPS=( $(aws iam list-entities-for-policy --policy-arn ${OLD_POLICY_ARN} |
 if [ ${#IAM_ROLES[@]} -gt 0 ]; then
     for i in ${!IAM_ROLES[@]}
         do
-            aws iam attach-role-policy --role-name ${IAM_ROLES[$i]} --policy-arn ${NEW_POLCIY_ARN}
+            aws iam attach-role-policy --role-name ${IAM_ROLES[$i]} --policy-arn ${NEW_POLICY_ARN}
             echo ${IAM_ROLES[$i]}
             aws iam detach-role-policy --role-name ${IAM_ROLES[$i]} --policy-arn ${OLD_POLICY_ARN}
             unset IAM_ROLES[$i]
@@ -24,7 +24,7 @@ fi
 if [ ${#IAM_USERS[@]} -gt 0 ]; then
     for i in ${!IAM_USERS[@]}
         do
-            aws iam attach-user-policy --user-name ${IAM_USERS[$i]} --policy-arn ${NEW_POLCIY_ARN}
+            aws iam attach-user-policy --user-name ${IAM_USERS[$i]} --policy-arn ${NEW_POLICY_ARN}
             echo ${IAM_USERS[$i]}
             aws iam detach-user-policy --user-name ${IAM_USERS[$i]} --policy-arn ${OLD_POLICY_ARN}
             unset IAM_USERS[$i]
@@ -37,7 +37,7 @@ fi
 if [ ${#IAM_GROUPS[@]} -gt 0 ]; then
     for i in ${!IAM_GROUPS[@]}
         do
-            aws iam attach-group-policy --group-name ${IAM_GROUPS[$i]} --policy-arn ${NEW_POLCIY_ARN}
+            aws iam attach-group-policy --group-name ${IAM_GROUPS[$i]} --policy-arn ${NEW_POLICY_ARN}
             echo ${IAM_GROUPS[$i]}
             aws iam detach-group-policy --group-name ${IAM_GROUPS[$i]} --policy-arn ${OLD_POLICY_ARN}
             unset IAM_GROUPS[$i]
